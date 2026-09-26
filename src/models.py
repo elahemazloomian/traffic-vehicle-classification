205f475bd48adc429b90407d4fd770183805fa3c
elahe mazloomian <96005309+elahemazloomian@users.noreply.github.com>
2026-09-26 23:14:09 +0330

    Update README with today's progress on fine-tuning and custom CNN architectures

--- src/models.py
+++ src/models.py
@@ -34,3 +34,92 @@ if __name__ == "__main__":
             
     if active_layers == 2:
         print("\n✅ Model successfully frozen, only the last layer is ready for training.")
+
+
+class SimpleTrafficCNN(nn.Module):
+    def __init__(self, num_classes=8):
+        super(SimpleTrafficCNN, self).__init__()
+        
+        
+        self.block1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2) 
+        )
+        
+        
+        self.block2 = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2) 
+        )
+        
+        
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * 56 * 56, 128),
+            nn.ReLU(),
+            nn.Dropout(0.5), 
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.classifier(x)
+        return x
+
+if __name__ == "__main__":
+    
+    model = SimpleTrafficCNN(num_classes=8)
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"Simple Custom CNN Total Parameters: {total_params:,}")
+
+
+
+class TrafficNetProficient(nn.Module):
+    def __init__(self, num_classes=8):
+        super(TrafficNetProficient, self).__init__()
+        
+    
+        self.feature_extractor = nn.Sequential(
+            
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2), 
+            
+            
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            
+            
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            
+            
+            nn.AdaptiveAvgPool2d((1, 1))
+        )
+        
+        
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            nn.Linear(64, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = self.classifier(x)
+        return x
+
+if __name__ == "__main__":
+    model = TrafficNetProficient(num_classes=8)
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"Proficient Custom CNN Total Parameters: {total_params:,}")
